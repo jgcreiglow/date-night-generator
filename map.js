02de559535f10757255a8de4af0bee1a4b0344d0
Max Cliff <maxcliff@Maxs-MacBook-Pro.local>
2018-03-08 14:30:28 -0500

    map stuff

--- map.js
+++ map.js
@@ -1,35 +1,23 @@
 import { text } from "../../Library/Caches/typescript/2.6/node_modules/@types/body-parser";
 
-function initMap() {
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: { lat: 38.9072, lng: -77.0369 },
-    zoom: 13
-  });
-}
-
-
-
 var map;
 var service;
-var infowindow;
+var infoWindow;
 
-function initialize() {
-  var WashDc = new google.maps.LatLng(-38.9072,-77.0369);
-
+function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
-      center: WashDc,
-      zoom: 15
-    });
-
+    center: { lat: 38.9072, lng: -77.0369 },
+    zoom: 13,
+  });
   var request = {
     location: WashDc,
     radius: '500',
-    query: 'Parks',
+    query: 'parks'
   };
-
-  service = new google.maps.places.PlacesService(map);
-  service.textSearch(request, callback);
 }
+  service = new google.maps.places.PlacesService(map);
+  service.nearbySearch(request, callback);
+
 
 function callback(results, status) {
   if (status == google.maps.places.PlacesServiceStatus.OK) {
@@ -39,48 +27,19 @@ function callback(results, status) {
     }
   }
 }
+function createMarker(place) {
+  var placeLoc = place.geometry.location;
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location
+  });
 
-var map;
-var service;
-var infowindow;
-
-function initialize() {
-  var WashDc = new google.maps.LatLng(-33.8665433,151.1956316);
-
-  map = new google.maps.Map(document.getElementById('map'), {
-      center: WashDc,
-      zoom: 15
-    });
-
-  var request = {
-    location: WashDc,
-    radius: '500',
-    query: 'Parks'
-  };
-
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.setContent(place.name);
+    infowindow.open(map, this);
+  });
 }
 
 
-// {
-//   search(term= "", location= "");{
-//     total
-//     business ;{
-//       name
-//       reviews
-//         text
-//         rating
-//         time_created
-//         url
-//     }
-//   }
-// }
 
-// $.curl -X POST -H "Authorization: Bearer ACCESS_TOKEN" -H "Content-Type: application/graphql" "https://api.yelp.com/v3/graphql" --data'
-// {
-//     business(id: "garaje-san-francisco") {
-//         name
-//         id
-//         rating
-//         url
-//     }
-// }'
+
